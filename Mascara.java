dc15cc08bde4b650bdf5098737c9f52c6a269ee1
Rodrigo Gilberto Marin Catto <iskigow@gmail.com>
2010-07-13 05:41:50 -0700

    Under BSD License

--- Mascara.java
+++ Mascara.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2007,2009-2010 Rodrigo G. M. Catto.
+ * Copyright (c) 2007-2010 Rodrigo G. M. Catto.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
